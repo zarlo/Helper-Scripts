0b5c1266d96cd93d1a0de115d6b8926b598cdf8b
zarlo5899@gmail.com <zarlo5899@gmail.com>
2018-05-10 15:04:50 +1000

    boop

--- ogp/Agent.sh
+++ ogp/Agent.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 dpkg --add-architecture i386
 apt-get update
 apt-get upgrade -y
@@ -9,12 +8,8 @@ apt-get install -y libstdc++6 libstdc++6:i386 default-jdk
 apt-get install -y libc6-i386 libgcc1:i386
 apt-get install -y lib32gcc1
 apt-get install -y libhttp-daemon-perl
-
 wget -N "https://github.com/OpenGamePanel/Easy-Installers/raw/master/Linux/Debian-Ubuntu/ogp-agent-latest.deb" -O "ogp-agent-latest.deb"
 dpkg -i "ogp-agent-latest.deb"
-
 apt-get install -f -y
-
 echo "Agent Info"
-
 cat /root/ogp_user_password
